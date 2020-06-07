a9cc857be01a98370bd96a951bdcfbf41bf38133
Zhunisali <zshanabek@gmail.com>
2020-06-07 21:32:18 +0600

    fix issues

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,19 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :update, :destroy]
+  DEFAULT_PAGE_SIZE = 3
 
   # GET /posts
   def index
-    limit = params[:count] ? Integer(params[:count]) : 3
-    cursor = params[:cursor] ? Integer(params[:cursor]) : 1
-    @posts = Post.where("id >= ?", cursor).limit(limit).order(:created_at)
-    next_cursor = cursor + limit >= Post.count ? "" : @posts.last.id + 1
-    render json: {"posts": @posts,"next_cursor": next_cursor}, status: :ok
+    limit = params[:page][:size] ? Integer(params[:page][:size]) : DEFAULT_PAGE_SIZE
+    direction = params[:page][:after] ? :after : :before
+    cursor = params[:page][direction] ? Integer(params[:page][direction]) : 1
+    @posts = direction == :after ? Post.where("id >= ?", cursor) : Post.where("id < ?", cursor)
+    @posts = @posts.limit(limit).order(:created_at)
+    @last_end = Post.where(:id => cursor..Post.last.id)
+    @last_beginning = Post.where(:id => Post.first.id..cursor)
+    next_cursor = limit < @last_end.count ? @posts.last.id + 1 : ""
+    prev_cursor = limit < @last_beginning.count ? @last_beginning.last.id - 1 : ""
+    render json: {"posts": @posts,"next_cursor": next_cursor,"prev_cursor": prev_cursor}, status: :ok
   end
 
   # GET /posts/1
